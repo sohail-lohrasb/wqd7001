3957bad8eea5e0ce0e883dd1d9dd6cfc27b099ac
sohail-lohrasb <sohail.lohrasb@gmail.com>
2018-12-17 14:19:44 +0800

    sg

---
+++ code/server.R
@@ -0,0 +1,233 @@
+#########################################################################
+#                                                                       #
+#############       Olympic Data Analysis        ########################
+
+#Members : 
+#1) Shukri
+#2) Sohail
+#3) Xu Xiang
+#4) Shobana
+
+#########################################################################
+
+library(shiny)
+library(shinydashboard)
+library(dplyr)
+library(DT)
+library(ggplot2)
+library(reshape2)
+library(ggrepel)
+
+########################## Load Data File and extract data frames for reporting #########
+
+joindata <- read.csv('JoinedData.csv')
+joindata$Population <- joindata$Population/1000000
+
+
+## Extract rows which have medals only and count of medals for country, year and sex 
+medaltally1 <- count(subset(joindata, is.na(joindata$Medal) == FALSE), Country,Year, Sex,Sport, Medal)
+
+
+## dcast is a function from the reshape2 library
+medaltally <- dcast(medaltally1, Country + Year + Sex + Sport ~ Medal, fill = 0)
+Top20 <- medaltally1 %>% group_by(Country) %>% 
+  summarise(totalMedal = sum(n)) %>% 
+  arrange(desc(totalMedal)) %>% 
+  head(20) %>% 
+  pull(Country)
+
+#########################  data preparation for  Graph ######################################
+
+d1 <- medaltally1   
+d1 <- group_by(medaltally1,Country)  %>%   summarise(sum(n)) %>% setNames(., c("Country" ,"TotMedals"))
+d2 <- inner_join(d1, distinct(joindata,Country, joindata$GDP....per.capita., Population), by = "Country") %>%
+  setNames(.,c("Country","Totmedals","Population","Percapita"))
+
+######################### Server Functions ##################################################
+
+server <- function(input, output) {
+
+#########################    DATA TABLE SELECTION  - SPORT ,  SEX and YEARS  #################    
+  
+  dat <- reactive({ 
+    temp <- medaltally
+    if (input$Sport[1] != "ALL") {
+      temp <- filter(medaltally, Sport %in% input$Sport) 
+    }
+    if (input$Sex != "ALL") {
+      temp <- filter(temp, Sex %in% input$Sex) 
+    }
+    temp <- subset(temp,Year >=  input$daterange[1] &  Year <= input$daterange[2])
+    temp
+  })
+  dat
+  dat1 <- reactive({
+    datx  <-subset(d1,d1$Year >=  input$daterange[1] & d1$Year <= input$daterange[2])
+  })          
+  dat1         
+  output$medalTally <- DT::renderDataTable({ 
+    DT::datatable(dat() , options = list(pageLength=10, pagingType = "full_numbers" )) %>% 
+      formatStyle(colnames(dat()), background = 'white')    
+  })
+  
+  
+#########################    MEDAL GRAPH SELECTION AND OUTPUT  ################################# 
+  
+  
+    dat2 <- reactive ({ 
+    temp1 <- medaltally1 
+    temp1$Medal <- factor(temp1$Medal, levels = c("Gold", "Silver", "Bronze"))
+    ###Syukri:Added code to limit the view to top 20 of accumulated medals
+    if (input$Sport[1] == "ALL") {
+      temp9 <- temp1 %>% group_by(Country) %>% 
+        summarise(totalMedal = sum(n)) %>% 
+        arrange(desc(totalMedal)) %>% 
+        head(30) %>% 
+        pull(Country)
+      temp1 <- temp1 %>% filter(Country %in% temp9)
+    }
+    else if(input$Sport[1] != "ALL") {
+      temp1 <- filter(temp1, Sport %in% input$Sport) 
+      temp9 <- temp1 %>% group_by(Country) %>% 
+        summarise(totalMedal = sum(n)) %>% 
+        arrange(desc(totalMedal)) %>% 
+        head(30) %>% 
+        pull(Country)
+      temp1 <- temp1 %>% filter(Country %in% temp9)
+    } 
+    
+    if (input$Sex[1] != "ALL") {
+      temp1 <- filter(temp1, Sex %in% input$Sex) 
+    } 
+          
+    temp1 <-subset(temp1, Year >=  input$daterange[1] & Year <= input$daterange[2])
+    temp1
+  })        
+  dat2     
+  
+###to be edit and filter...Syukri:done pls see above instead. I change the temp1 input1 by adding if else statement
+  output$tallyGraph <- renderPlot({  
+    
+    ggplot(dat2(), aes(x=Country, y= n, fill = Medal))  + geom_bar(stat = "identity") +
+      theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+      ylab("Total Medals")
+    
+  } )             
+  
+  
+#############################  PLOT - MEDALS AND POPULATION RANGE #############################
+  
+  dat3 <- reactive ({ 
+    temp4 <-subset(d2, Population  <= input$Poprange[2]  & Population  >= input$Poprange[1] )
+    temp4  <- subset(temp4, Totmedals  >= input$Medalrange[1]   & Totmedals <= input$Medalrange[2])
+    temp4
+  })        
+  dat3                           
+  # Population Graph vs Total Medals 
+   output$Population <- renderPlot({
+    ggplot(dat3(), aes(x=Population, y= Totmedals)) + geom_point(color = 'red') + geom_text_repel(label=  dat3()$Country) + 
+      theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+      ylab("Total Medals")
+  } )                
+    
+   
+  output$medal1Plot <- renderPlot({
+    ggplot(dat2(), aes(x=Country, y= n, fill = Medal)) + geom_bar(stat = "identity") +
+      theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+      ylab("Total Medals")
+  })
+  
+   
+#############################  PLOT - MEDALS AND PER CAPITA  RANGE #############################
+   
+  dat4 <- reactive ({ 
+    
+    temp5 <-subset(d2, Percapita  <= input$Percapita[2]  & Population  >= input$Percapita[1] )
+    temp5  <- subset(d2, Totmedals  >= input$Medalrange[1]   & Totmedals <= input$Medalrange[2])
+    temp5
+  })        
+  dat4    
+  
+ # Graph for Total medals vs GDP Per capita 
+  output$perCapita <- renderPlot({
+    
+    ggplot(dat4(), aes(x=Percapita, y= Totmedals)) + geom_point(color = 'blue')    + geom_text_repel(label=  dat4()$Country) +
+      #                    ggplot(dat2(), aes(x=Country, y= n, fill = Medal)) + geom_bar(stat = "identity") +
+      
+      theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+      ylab("Total Medals") 
+      
+  } )                
+  
+
+  
+###############################  Prepare Data for Regression ############################### 
+  
+  #list data for all countries with 0 or more total medals  
+  medaltally2 <- count(joindata, Country, Year,Medal,Population,`GDP....per.capita.`)
+  
+  #count NA   
+  f1  = function(df, y)  { 
+    if    (is.na(df[3]) ) { 
+      df[6] <- as.numeric(0)  
+    } 
+    else {  df[6] <- as.numeric(df[6])}
+  }
+  
+  medaltally2[6] <- apply(medaltally2,1,f1)
+  
+  #final count for plotting               
+  medaltally3 <- group_by(medaltally2,Country,Year,Population, `GDP....per.capita.`) %>%  summarise(sum(n)) 
+  
+  
+############################### PLot -  Regression Plots  ###############################
+  
+  output$Regresspop   <- renderPlot   ({ 
+    ggplot(medaltally3, aes(x = Population, y = `sum(n)`, color = Year))  + geom_point() + scale_color_gradient(low="blue", high="red") + geom_smooth(method = lm) +
+      labs(title = "Medals vs Population",  x= "Population In Millions", y  = "Total Medals")
+  })
+  output$Regresscap   <- renderPlot   ({
+    ggplot(medaltally3, aes(x = `GDP....per.capita.`, y = `sum(n)`,color = Year))  + geom_point() +geom_smooth(method = lm)  +
+      labs(title = "Medals vs GDP Per Capita ",  x= "GDP Per Capita", y  = "Total Medals")
+  })  
+  
+##############################   Predictions   ############################################
+  
+#Predictions - Population 
+  
+  newpop <-  reactive({ 
+    temp <- unlist(strsplit(input$Popdata, split = ","))
+    tempx <- data.frame(Population = temp)
+    tempx[,'Population'] <- as.numeric(as.character(tempx[,'Population']))
+    tempx
+  })
+  predpop <- lm(`sum(n)`~ Population,data = medaltally3)
+  predcap <- lm(`sum(n)`~ `GDP....per.capita.` ,data = medaltally3)
+  output$Popresults <-renderTable ({ #newpop()
+    predict(predpop,newpop())
+  })
+  
+#Predictions - GDP Per Capita  
+  newcap <-  reactive({ 
+    temp <- unlist(strsplit(input$Capdata, split = ","))
+    tempx <- data.frame(`GDP....per.capita.` = temp)
+    tempx[,'GDP....per.capita.'] <- as.numeric(as.character(tempx[,'GDP....per.capita.']))
+    tempx
+  })
+
+  output$Capresults <-renderTable ({   
+    predict(predcap,newcap())
+  })
+   
+  ##############################   Help   ############################################
+  
+   url <- a("Help Page here", href="https://www.google.com/")
+    helpdoc <- ("This application guide... abcdefgh... << xu xiang >>")
+  output$helptext <- renderText({
+    print(helpdoc)
+  })  
+  output$helpui <-   renderUI({
+    tagList("***", url)
+  })
+} 
+               
---
+++ code/ui.R
@@ -0,0 +1,119 @@
+
+##################### load libraries ###################################.
+
+library(shiny)
+library(shinydashboard)
+library(dplyr)
+library(DT)
+library(ggplot2)
+library(reshape2)
+library(ggrepel)
+
+########################## Load Data File and extract data frames for reporting #########
+
+joindata <- read.csv('JoinedData.csv')
+joindata$Population <- joindata$Population/1000000 # scale population for analysis 
+
+## Extract rows which have medals only and count of medals for country, year and sex 
+medaltally1 <- count(subset(joindata, is.na(joindata$Medal) == FALSE), Country,Year, Sex,Sport, Medal)
+# Syukri reorder the level of Medal
+medaltally1$Medal <- factor(medaltally1$Medal, levels = c("Gold","Silver","Bronze"))
+
+
+## dcast is a function from the reshape2 library to bring it to wide format where separate columns for gold sliver bronze are there
+medaltally <- dcast(medaltally1, Country + Year + Sex + Sport ~ Medal, fill = 0)
+
+
+##################  data preparation  for  Medal Graph ##############################################
+
+d1 <- medaltally1   
+d1 <- group_by(medaltally1,Country)  %>%   summarise(sum(n)) %>% setNames(., c("Country" ,"TotMedals"))
+d2 <- inner_join(d1, distinct(joindata,Country, joindata$GDP....per.capita., Population), by = "Country") %>%
+  setNames(.,c("Country","Totmedals","Population","Percapita"))
+
+
+################### Olympics UI File ########################################################
+ui <- dashboardPage(skin = "blue",
+                    dashboardHeader(title = "Summer Olympics Analysis"),
+                    dashboardSidebar( width = 120, 
+                                      sidebarMenu(
+                                        #menuItem(".", tabName = "blank", icon = icon(" ")),
+                                        menuItem("Graphs", tabName = "graphs", icon = icon("chart-bar"), selected = TRUE),
+                                        menuItem("XYPlots", tabName = "xyplots", icon = icon("chart-line")) ,
+                                        menuItem("Analysis", tabName = "analysis", icon = icon("lightbulb")) ,
+                                        menuItem("Help", tabName = "help", icon = icon("question"))  
+                                        
+                                      )
+                    ) ,
+                    
+                    dashboardBody(
+                      #  tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "bootstrap.css")),
+                      tags$head(tags$style(HTML('.content-wrapper {background-color:black;}')                                            )), 
+                      tabItems( 
+                        tabItem(tabName = "graphs",
+                                fluidRow(
+                                  box(color = "black",title =  "Medal Tally", width =6, DT::dataTableOutput("medalTally")), 
+                                  box(title = "Medal Tally", width = 6, plotOutput("tallyGraph" , height = 500, width = 620) )  
+                                ) , 
+                                fluidRow( 
+                                  box(width = 12, background= "black", color = "black", title = "  "),     
+                                  box(width = 8,color = "black",
+                                      checkboxGroupInput("Sport",  "Sport",  choices = c("ALL",unique(as.character(medaltally$Sport)) ),width = 800, inline = TRUE,selected = "ALL" ) 
+                                  ) , 
+                                  box(width = 2, color = "black",sliderInput("daterange", "Year:",
+                                                                             min = min(medaltally$Year), max = max(medaltally$Year),
+                                                                             value = c(2004,2016),width = 250))  ,
+                                  box(width = 1,color = "black",
+                                      checkboxGroupInput("Sex",  "Sex",  choices = c("ALL",unique(as.character(medaltally$Sex)) ), selected = "ALL",width =20 )
+                              ) 
+                                )       
+                        ) , 
+                        
+                        tabItem(tabName = "xyplots",
+                        fluidRow(
+                          box(title = "Medals vs Population", width = 6, plotOutput("Population", height = 320, width = 580)) , 
+                            box(color ="black",title = "Medals vs GDP Per Capita", width = 6, plotOutput("perCapita" , height = 320, width = 580) )
+                        ),
+                        fluidRow( 
+                          box(width = 12, background= "black", color = "black", title = "      "),
+                          
+                          box(width = 3, color = "black",sliderInput("Poprange", "Population in Million:",
+                                                                   min = min(d2$Population), max = max(d2$Population),
+                                                                   value = c(20,1400)))  ,                
+                        box(width = 3, color = "black", sliderInput("Percapita", "GDP Per Capita:",
+                                                                    min = min(d2$Percapita), max = max(d2$Percapita),
+                                                                    value = c(4000,100000)))  , 
+                        
+                        box(width = 3, color = "black",sliderInput("Medalrange", "Total Medals:",
+                                                                   min = min(d2$Totmedals), max = max(d2$Totmedals),
+                                                                   value = c(30,8000)))       
+                          )
+                        
+                        ), 
+                        
+                        tabItem(tabName = "analysis",
+                                fluidRow(
+                                  box(width = 6, plotOutput("Regresspop" ,  width = 550)) , 
+                                  box( color = 'black' , width =6, plotOutput("Regresscap",width = 550)) ,   
+                                  box(width = 6, color = 'black', textInput("Popdata", label =  "Enter Population  Data in Millions",value = '23,36,42' )),
+                                  box(color = 'black', textInput("Capdata", label =  "Enter GDP Per Capita  Data ",value = '23000,36000,82000' )),
+                                  box( color = 'black',title = "Prediction Medals - Population Millions", width =3, tableOutput("Popresults"))  , 
+                                  box( color = 'black',title = "Prediction Medals - GDP Per Capita", width =3, tableOutput("Capresults"))   
+                                )
+                        )  ,
+                        
+                        tabItem(tabName = "help", 
+                                
+                                fluidRow(
+                                   box(width = 12, height = 600, textOutput("helptext")),
+                                   box(width = 5 , background = "yellow", color = "blue", uiOutput("helpui"))   
+                                  
+                                )
+                                
+                                )
+                      )  
+                    )   
+            
+)
+
+
